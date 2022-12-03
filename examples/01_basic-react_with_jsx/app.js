5bc1294808bef0e175598ce053a08a469f09d06e
amisegevs <ami.seg44@gmail.com>
2022-12-03 10:23:45 +0200

    add new react app using create app-practice at hom

---
+++ examples/01_basic-react_with_jsx/app.js
@@ -0,0 +1,13 @@
+
+function Hello() {
+    return (
+        <div>
+            <div>Hello to jsx</div>    
+            <p>This is paragraph</p>
+        </div>
+    )
+}
+
+const rootNode = document.getElementById('root');
+const root = ReactDOM.createRoot(rootNode);
+root.render(React.createElement(Hello));
